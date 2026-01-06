012ad2a9ea5b80e5ea6479070bc758b0753c6a70
Jake Soenneker <jake@soenneker.com>
2026-01-06 12:57:14 -0600

    Improves performance and allocation

--- src/ConcurrentCircularQueue.cs
+++ src/ConcurrentCircularQueue.cs
@@ -1,7 +1,8 @@
 using Soenneker.Utils.ConcurrentCircularQueue.Abstract;
-using System.Collections.Concurrent;
 using System;
-using System.Linq;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Soenneker.Asyncs.Locks;
 using Soenneker.Extensions.ValueTask;
@@ -10,7 +11,7 @@ using Soenneker.Atomics.ValueInts;
 namespace Soenneker.Utils.ConcurrentCircularQueue;
 
 /// <inheritdoc cref="IConcurrentCircularQueue{T}"/>
-public class ConcurrentCircularQueue<T> : IConcurrentCircularQueue<T>
+public sealed class ConcurrentCircularQueue<T> : IConcurrentCircularQueue<T>
 {
     private readonly ConcurrentQueue<T> _queue = new();
     private readonly int _maxSize;
@@ -23,40 +24,67 @@ public class ConcurrentCircularQueue<T> : IConcurrentCircularQueue<T>
     public ConcurrentCircularQueue(int maxSize, bool locking = false)
     {
         if (maxSize <= 0)
-            throw new ArgumentException("MaxSize must be greater than zero.");
+            throw new ArgumentException("MaxSize must be greater than zero.", nameof(maxSize));
 
         _maxSize = maxSize;
         _locking = locking;
 
-        if (_locking)
+        if (locking)
             _asyncLock = new AsyncLock();
 
         _count = new ValueAtomicInt(0);
     }
 
-    public async ValueTask<bool> Contains(T item)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ValueTask<bool> Contains(T item)
     {
         if (!_locking)
-            return _queue.Contains(item);
+            return new ValueTask<bool>(QueueContains(item));
 
-        using (await _asyncLock!.Lock()
-                                .NoSync())
-            return _queue.Contains(item);
+        return ContainsLocked(item);
     }
 
-    public async ValueTask Enqueue(T item)
+    private async ValueTask<bool> ContainsLocked(T item)
+    {
+        using (await _asyncLock!.Lock()
+                                .NoSync())
+            return QueueContains(item);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool QueueContains(T item)
+    {
+        var comparer = EqualityComparer<T>.Default;
+
+        foreach (T current in _queue)
+        {
+            if (comparer.Equals(current, item))
+                return true;
+        }
+
+        return false;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ValueTask Enqueue(T item)
     {
         if (!_locking)
         {
             EnqueueInternal(item);
-            return;
+            return ValueTask.CompletedTask;
         }
 
+        return EnqueueLocked(item);
+    }
+
+    private async ValueTask EnqueueLocked(T item)
+    {
         using (await _asyncLock!.Lock()
                                 .NoSync())
             EnqueueInternal(item);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private void EnqueueInternal(T item)
     {
         _queue.Enqueue(item);
@@ -70,14 +98,20 @@ public class ConcurrentCircularQueue<T> : IConcurrentCircularQueue<T>
         }
     }
 
-    public async ValueTask<(bool success, T? result)> TryDequeue()
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ValueTask<(bool success, T? result)> TryDequeue()
     {
         if (!_locking)
         {
             bool success = TryDequeueInternal(out T? result);
-            return (success, result);
+            return new ValueTask<(bool success, T? result)>((success, result));
         }
 
+        return TryDequeueLocked();
+    }
+
+    private async ValueTask<(bool success, T? result)> TryDequeueLocked()
+    {
         using (await _asyncLock!.Lock()
                                 .NoSync())
         {
@@ -86,6 +120,7 @@ public class ConcurrentCircularQueue<T> : IConcurrentCircularQueue<T>
         }
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private bool TryDequeueInternal(out T result)
     {
         bool dequeueResult = _queue.TryDequeue(out result);
@@ -96,11 +131,17 @@ public class ConcurrentCircularQueue<T> : IConcurrentCircularQueue<T>
         return dequeueResult;
     }
 
-    public async ValueTask<int> Count()
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ValueTask<int> Count()
     {
         if (!_locking)
-            return _count.Read();
+            return new ValueTask<int>(_count.Read());
 
+        return CountLocked();
+    }
+
+    private async ValueTask<int> CountLocked()
+    {
         using (await _asyncLock!.Lock()
                                 .NoSync())
             return _count.Read();
